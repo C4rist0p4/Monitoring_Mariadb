46439c448191adfb142a34082289624cec8ed0b1
C4rist0p4 <Christoph_S@posteo.de>
2020-03-31 19:18:49 +0200

    commit 1

---
+++ index.js
@@ -0,0 +1,63 @@
+const MySQLEvents = require('@rodrigogs/mysql-events');
+const dotenv = require('dotenv');
+var admin = require("firebase-admin");
+dotenv.config();
+
+
+var serviceAccount = require(process.env.serviceAccount);
+
+  const program = async () => {
+    const instance = new MySQLEvents({
+      host: process.env.host,
+      user: process.env.user,
+      password: process.env.password,
+    }, {
+      startAtEnd: true,
+    });
+  
+    
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.databaseURL
+    });
+
+    
+    await instance.start();
+  
+    instance.addTrigger({
+      name: 'monitoring all INSERT',
+      expression: 'flussskraftwerk.meldungen.*',
+      statement: MySQLEvents.STATEMENTS.INSERT,
+      onEvent: (event) => {
+        console.log(event.affectedRows[0].after)
+        
+        //sendPushNotification("event"); 
+      },
+    });
+
+    function sendPushNotification(msg) {
+      var registrationToken = 'esqEZCjkoP8:APA91bG07IxgdTTfeHynAJgwjeHq6WeKWFbl8MiUiuVmp4FGvVemhpJSBGKf4a3PqesCYmUMAjMJJZu0arGpG3I5Eryv1REOVTAfq95CPKW2KgZ_y3Kxg2xddEqlEx4-jPz6_J898iQt';
+
+      var message = {
+        notification: {
+          title: 'event',
+          body: '2:45'
+        }
+      };
+
+      admin.messaging().sendToDevice(registrationToken, message)
+      .then((response) => {
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+    };
+  
+    instance.on(MySQLEvents.EVENTS.CONNECTION_ERROR, console.error);
+    instance.on(MySQLEvents.EVENTS.ZONGJI_ERROR, console.error);
+  };
+  
+  program()
+    .then(() => console.log('Waiting for database evnts...'))
+    .catch(console.error);
