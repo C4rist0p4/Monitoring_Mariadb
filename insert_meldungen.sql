fa65caddaaca8f415cb083e91ed8d5ca59b07c3c
C4rist0p4 <Christoph_S@posteo.de>
2020-08-25 21:17:10 +0200

    25

--- insert_meldungen.sql
+++ insert_meldungen.sql
@@ -1,2 +1,2 @@
 INSERT INTO `flusskraftwerk`.`meldungen`  (fk_anlagen, fk_meldungstyp, datum, bemerkungMel,	timestamp_device)
-VALUES ('1', '3', '2020-4-17', 'Schaltschranktuer wurde geoeffnet', '2020-4-17');
+VALUES ('1', '3', '2020-06-26', 'Schaltschranktuer wurde geoeffnet!!!', '2020-06-21');
